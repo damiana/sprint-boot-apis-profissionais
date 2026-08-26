edda91af0db6b6e9c6326e81f6a71924ae2f93e7
Damiana Costa <damiana.costa@yahoo.com.br>
2026-08-26 12:04:00 -0300

    aula 8.2: configurando Spring Profiles (application-dev/prod)

--- src/test/java/br/com/alura/runnercircleapi/RunnerCircleApiApplicationTests.java
+++ src/test/java/br/com/alura/runnercircleapi/RunnerCircleApiApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.alura.runnercircleapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RunnerCircleApiApplicationTests {
 
 	@Test
